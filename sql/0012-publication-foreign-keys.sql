27b725e6a776fc79b61b561836ff3cca4d8c6aee
Peter Hajtol <hajtopet@fit.cvut.cz>
2019-02-12 00:29:11 +0100

    fix database name in database migration 12

--- sql/0012-publication-foreign-keys.sql
+++ sql/0012-publication-foreign-keys.sql
@@ -1,19 +1,19 @@
 SET FOREIGN_KEY_CHECKS=0;
 
-ALTER TABLE `pubconf`.`publication` 
+ALTER TABLE `publication` 
 ADD CONSTRAINT `publication_journal_id_FK`
   FOREIGN KEY (`journal_id`)
-  REFERENCES `pubconf`.`journal` (`id`)
+  REFERENCES `journal` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
 ADD CONSTRAINT `publication_publisher_id_FK`
   FOREIGN KEY (`publisher_id`)
-  REFERENCES `pubconf`.`publisher` (`id`)
+  REFERENCES `publisher` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
 ADD CONSTRAINT `publication_conference_year_id_FK`
   FOREIGN KEY (`conference_year_id`)
-  REFERENCES `pubconf`.`conference_year` (`id`)
+  REFERENCES `conference_year` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
